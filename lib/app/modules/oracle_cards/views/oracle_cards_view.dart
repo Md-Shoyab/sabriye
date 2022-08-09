eeb415d939eca8e6722b35ad6b8f5067754a7ab4
Md Shoyab <mdshoyab06@gmail.com>
2022-08-09 20:57:21 +0530

    all other screen for navigation created

--- lib/app/modules/oracle_cards/views/oracle_cards_view.dart
+++ lib/app/modules/oracle_cards/views/oracle_cards_view.dart
@@ -6,13 +6,12 @@ class OracleCardsView extends GetView<OracleCardsController> {
   const OracleCardsView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      body: Image.network(
-        'https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067__340.png',
-        height: Get.height,
-        width: Get.width,
-        fit: BoxFit.cover,
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'OracleCardsView',
+          style: TextStyle(fontSize: 30),
+        ),
       ),
     );
   }
