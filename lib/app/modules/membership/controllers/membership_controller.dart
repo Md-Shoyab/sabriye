e3d1e4dddc9dd244152008f96a48522d444fa249
Md Shoyab <mdshoyab06@gmail.com>
2022-12-30 10:55:22 +0530

    remove unecessary model

--- lib/app/modules/membership/controllers/membership_controller.dart
+++ lib/app/modules/membership/controllers/membership_controller.dart
@@ -1,9 +1,7 @@
 import 'package:get/get.dart';
-import 'package:sabriye/model/membership_accordion.dart';
 import 'package:sabriye/services/api_services.dart';
 
 class MembershipController extends GetxController {
-  late List<MemberShipAccordionItem> membershipAccordionItem;
   final ApiServices _apiServices = ApiServices();
   late Uri lifetimePlanUri = Uri.parse(lifetimePlanUrl.value);
   late Uri annualPlanUri = Uri.parse(annualPlanUrl.value);
--- lib/model/membership_accordion.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/state_manager.dart';
-
-class MemberShipAccordionItem {
-  String? header;
-  String? body;
-  RxBool? isExpanded;
-
-  MemberShipAccordionItem(this.header, this.body, this.isExpanded);
-}
